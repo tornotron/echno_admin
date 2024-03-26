8ee9ff5b824472ce2bc0e27c7d50b6bb6302c045
Abin K Johny <abinkjohny2011@gmail.com>
2024-03-26 22:13:51 +0530

    Update auth_exceptions.dart with additional error messages from EchnoExceptions

--- lib/utilities/exceptions/auth_exceptions.dart
+++ lib/utilities/exceptions/auth_exceptions.dart
@@ -102,6 +102,18 @@ class EchnoFirebaseAuthException implements Exception {
         return 'An internal authentication error occurred. Please try again later.';
       case 'INVALID_LOGIN_CREDENTIALS':
         return 'Invalid login credentials.';
+      case 'too-many-requests':
+        return 'Too many requests. Please try again later.';
+      case 'invalid-argument':
+        return 'Invalid argument provided to the authentication method.';
+      case 'invalid-password':
+        return 'Incorrect password. Please try again.';
+      case 'invalid-phone-number':
+        return 'The provided phone number is invalid.';
+      case 'sign_in_failed':
+        return 'Sign-in failed. Please try again.';
+      case 'network-request-failed':
+        return 'Network request failed. Please check your internet connection.';
       default:
         return 'An unexpected authentication error occurred. Please try again.';
     }
