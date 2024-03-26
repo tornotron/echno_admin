4d1f09474b08218d2b5a6d22feca22f318c2ee9d
Abin K Johny <abinkjohny2011@gmail.com>
2024-03-26 21:50:54 +0530

    Removed unused routes

--- lib/utilities/routes.dart
+++
@@ -1,5 +0,0 @@
-const loginRoute = "/login/";
-const registerRoute = "/register/";
-const verifyEmailRoute = "/verify_email/";
-const homeRoute = "/home/";
-const resetPasswordRoute = "/reset_password/";
