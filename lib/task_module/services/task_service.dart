ed6ac6333c36224d9eaf16c72e3d35f16c65aec2
Abin K Johny <abinkjohny2011@gmail.com>
2024-04-01 21:40:13 +0530

    Add TaskService class and firestore backend logics

---
+++ lib/task_module/services/task_service.dart
@@ -0,0 +1,98 @@
+import 'package:echno_attendance/task_module/domain/firestore/firestore_task_handler.dart';
+import 'package:echno_attendance/task_module/domain/firestore/task_handler.dart';
+import 'package:echno_attendance/task_module/models/task_model.dart';
+
+class TaskService implements TaskHandler {
+  final TaskHandler _taskHandler;
+  const TaskService(this._taskHandler);
+
+  factory TaskService.firestoreTasks() {
+    return TaskService(FirestoreTaskHandler());
+  }
+
+  @override
+  Future<Task?> addNewTask({
+    required String title,
+    required String description,
+    required DateTime createdAt,
+    required DateTime? startDate,
+    required DateTime? endDate,
+    required String taskAuthor,
+    required String? taskType,
+    required String? taskStatus,
+    required double taskProgress,
+    required String? assignedEmployee,
+    required String? siteOffice,
+  }) {
+    return _taskHandler.addNewTask(
+      title: title,
+      description: description,
+      createdAt: createdAt,
+      startDate: startDate,
+      endDate: endDate,
+      taskAuthor: taskAuthor,
+      taskType: taskType,
+      taskStatus: taskStatus,
+      taskProgress: taskProgress,
+      assignedEmployee: assignedEmployee,
+      siteOffice: siteOffice,
+    );
+  }
+
+  @override
+  Future<void> updateTask({
+    required String taskId,
+    String? newTitle,
+    String? newDescription,
+    DateTime? newStartDate,
+    DateTime? newEndDate,
+    String? newTaskType,
+    String? newTaskStatus,
+    double? newTaskProgress,
+    String? newAssignedEmployee,
+    String? newSiteOffice,
+  }) {
+    return _taskHandler.updateTask(
+      taskId: taskId,
+      newTitle: newTitle,
+      newDescription: newDescription,
+      newStartDate: newStartDate,
+      newEndDate: newEndDate,
+      newTaskType: newTaskType,
+      newTaskStatus: newTaskStatus,
+      newTaskProgress: newTaskProgress,
+      newAssignedEmployee: newAssignedEmployee,
+      newSiteOffice: newSiteOffice,
+    );
+  }
+
+  @override
+  Future<void> updateTaskProgress({
+    required String taskId,
+    double? newTaskProgress,
+    String? newTaskStatus,
+  }) {
+    return _taskHandler.updateTaskProgress(
+      taskId: taskId,
+      newTaskProgress: newTaskProgress,
+      newTaskStatus: newTaskStatus,
+    );
+  }
+
+  @override
+  Future<void> deleteTask({required String taskId}) {
+    return _taskHandler.deleteTask(taskId: taskId);
+  }
+
+  @override
+  Stream<List<Task>> streamTasksByEmployee(
+      {required String? assignedEmployee}) {
+    return _taskHandler.streamTasksByEmployee(
+        assignedEmployee: assignedEmployee);
+  }
+
+  @override
+  Stream<List<Task>> streamTasksBySiteOffice({required String? siteOffice}) {
+    return _taskHandler.streamTasksBySiteOffice(siteOffice: siteOffice);
+  }
+}
