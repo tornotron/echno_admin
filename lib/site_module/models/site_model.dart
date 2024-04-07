9e325d930a42545245be53909879f9820ee1cd72
Abin K Johny <abinkjohny2011@gmail.com>
2024-04-07 22:49:25 +0530

    Redefined site_office model

--- lib/site_module/models/site_model.dart
+++ lib/site_module/models/site_model.dart
@@ -1,47 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:echno_attendance/site_module/utilities/site_status.dart';
-import 'package:echno_attendance/employee/models/employee.dart';
-import 'package:echno_attendance/employee/services/employee_service.dart';
 import 'package:echno_attendance/logger.dart';
 import 'package:logger/logger.dart';
 
 class SiteOffice {
   final logs = logger(SiteOffice, Level.info);
   final String siteOfficeName;
-  final SiteStatus status;
-  final double longitude;
-  final double latitude;
-  final double radius;
-  List<Employee> membersList;
+  final String siteAddress;
+  final SiteStatus siteStatus;
+  final double siteLongitude;
+  final double siteLatitude;
+  final double siteRadius;
+  List<String> membersList;
 
   SiteOffice({
     required this.siteOfficeName,
-    required this.status,
-    required this.longitude,
-    required this.latitude,
+    required this.siteAddress,
+    required this.siteStatus,
+    required this.siteLongitude,
+    required this.siteLatitude,
+    required this.siteRadius,
     required this.membersList,
-    required this.radius,
   });
 
-  Future<void> populateMemberlist() async {
-    try {
-      DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
-          .collection('site')
-          .doc(siteOfficeName)
-          .get();
-      List<dynamic> empidList = documentSnapshot['employee-list'];
-
-      for (String emp in empidList) {
-        final empService = EmployeeService.firestore();
-        Employee? employee = await empService.readEmployee(employeeId: emp);
-        if (employee != null) {
-          membersList.add(employee);
-        }
-      }
-    } on FirebaseException catch (error) {
-      logs.e('Firebase Exception: ${error.message}');
-    } catch (e) {
-      logs.e('Other Exception: $e');
-    }
+  factory SiteOffice.fromFirestore(QueryDocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return SiteOffice(
+      siteOfficeName: data['site-name'] ?? '',
+      siteAddress: data['site-address'] ?? '',
+      siteStatus: getSiteStatus(data['site-status']) ?? SiteStatus.upcoming,
+      siteLongitude: data['site-longitude'] ?? 0.0,
+      siteLatitude: data['site-latitude'] ?? 0.0,
+      siteRadius: data['site-radius'] ?? 0.0,
+      membersList: List<String>.from(data['employee-list']),
+    );
   }
 }
