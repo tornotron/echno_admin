5997c95b1e72d26ac19e7c6b9ee8c49b3aa564a9
Abin K Johny <abinkjohny2011@gmail.com>
2024-04-07 13:35:44 +0530

    Add SiteHandler interface for site management operations

---
+++ lib/site_module/domain/firestore/site_handler.dart
@@ -0,0 +1,33 @@
+import 'package:echno_attendance/attendance/utilities/site_status.dart';
+
+abstract class SiteHandler {
+  Future<void> createSiteOffice({
+    required String siteName,
+    required SiteStatus siteStatus,
+    required String siteAddress,
+    required double siteLatitude,
+    required double siteLongitude,
+    required double siteRadius,
+    required List<String>? memberList,
+  });
+
+  Future<void> updateSiteOffice({
+    required String siteName,
+    required SiteStatus? siteStatus,
+    required String? siteAddress,
+    required double? siteLatitude,
+    required double? siteLongitude,
+    required double? siteRadius,
+    required List<String>? memberList,
+  });
+
+  Future<void> addSiteMember({
+    required String siteName,
+    required List<String>? memberList,
+  });
+
+  Future<void> removeSiteMember({
+    required String siteName,
+    required List<String>? memberList,
+  });
+}
