4c2085a804fcd90402bdc1802cf597bc1b3888eb
Abin K Johny <abinkjohny2011@gmail.com>
2024-04-15 14:25:40 +0530

    Added floating button instead of appbar actions

--- lib/site_module/screens/site_management_screen.dart
+++ lib/site_module/screens/site_management_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:echno_attendance/constants/colors.dart';
 import 'package:echno_attendance/site_module/models/site_model.dart';
 import 'package:echno_attendance/site_module/screens/create_site_screen.dart';
 import 'package:echno_attendance/common_widgets/custom_app_bar.dart';
@@ -34,118 +35,99 @@ class _SiteManageState extends State<SiteManage> {
     super.initState();
   }
 
-  void selectedItem(BuildContext context, item) {
-    switch (item) {
-      case 0:
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return const CreateSiteScreen();
-        }));
-        break;
-      case 1:
-        break;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: EchnoAppBar(
-        leadingIcon: Icons.arrow_back_ios_new,
-        leadingOnPressed: () {
-          Navigator.pop(context);
-        },
-        title: Text('Site Management',
-            style: Theme.of(context).textTheme.headlineSmall),
-        actions: [
-          PopupMenuButton<int>(
-            itemBuilder: (context) => [
-              PopupMenuItem<int>(
-                value: 0,
-                child: Text('Create new site',
-                    style: Theme.of(context).textTheme.bodyLarge),
-              ),
-              PopupMenuItem<int>(
-                value: 1,
-                child: Text('Delete site',
-                    style: Theme.of(context).textTheme.bodyLarge),
-              ),
-            ],
-            onSelected: (item) {
-              selectedItem(context, item);
-            },
-          )
-        ],
-      ),
-      body: StreamBuilder<List<SiteOffice>>(
-        stream: SiteService.firestore().fetchSiteOffices(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(
-              child: Text(
-                'No site data found...',
-                style: Theme.of(context).textTheme.titleMedium,
-                textAlign: TextAlign.center,
-              ),
-            );
-          } else {
-            final List<SiteOffice> siteOffices = snapshot.data!;
+        appBar: EchnoAppBar(
+          leadingIcon: Icons.arrow_back_ios_new,
+          leadingOnPressed: () {
+            Navigator.pop(context);
+          },
+          title: Text('Site Management',
+              style: Theme.of(context).textTheme.headlineSmall),
+        ),
+        body: StreamBuilder<List<SiteOffice>>(
+          stream: SiteService.firestore().fetchSiteOffices(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(
+                child: Text(
+                  'No site data found...',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else {
+              final List<SiteOffice> siteOffices = snapshot.data!;
 
-            return GridView.builder(
-              itemCount: siteOffices.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2),
-              itemBuilder: (context, index) {
-                final siteOffice = siteOffices[index];
-                return Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.grey,
-                      image:
-                          DecorationImage(image: siteImage, fit: BoxFit.cover),
-                    ),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        InkWell(
-                          onLongPress: () {},
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return SiteHomeScreen(siteOffice: siteOffice);
-                                },
-                              ),
-                            );
-                          },
-                        ),
-                        Text(
-                          siteOffice.siteOfficeName,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 24,
+              return GridView.builder(
+                itemCount: siteOffices.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  final siteOffice = siteOffices[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: siteImage, fit: BoxFit.cover),
+                      ),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          InkWell(
+                            onLongPress: () {},
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return SiteHomeScreen(
+                                        siteOffice: siteOffice);
+                                  },
+                                ),
+                              );
+                            },
                           ),
-                        )
-                      ],
+                          Text(
+                            siteOffice.siteOfficeName,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              },
-            );
-          }
-        },
-      ),
-    );
+                  );
+                },
+              );
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: EchnoColors.selectedNavLight,
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return const CreateSiteScreen();
+            }));
+          },
+          child: const Icon(
+            Icons.add,
+            color: EchnoColors.white,
+          ),
+        ));
   }
 }
