b555ecf9cbee1fc951df72dc86f2bdb15d4f499d
Abin K Johny <abinkjohny2011@gmail.com>
2024-04-18 12:35:13 +0530

    Added script to enble firebase web in index.html file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:echno_attendance/echno.dart';
+import 'package:echno_attendance/utilities/index.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   // Initialize Firebase and the Flutter app.
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const EchnoApp());
 }
