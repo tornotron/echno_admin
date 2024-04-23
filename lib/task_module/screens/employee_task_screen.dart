3247c8a687e61c1bc3b93fa43198dbec24181bb1
Abin K Johny <abinkjohny2011@gmail.com>
2024-04-23 14:24:27 +0530

    Changed design for category selector in task home screens

--- lib/task_module/screens/employee_task_screen.dart
+++ lib/task_module/screens/employee_task_screen.dart
@@ -4,7 +4,9 @@ import 'package:echno_attendance/employee/models/employee.dart';
 import 'package:echno_attendance/task_module/services/task_service.dart';
 import 'package:echno_attendance/task_module/utilities/task_status.dart';
 import 'package:echno_attendance/task_module/widgets/employee_task_widgets/employee_task_stream.dart';
+import 'package:echno_attendance/utilities/helpers/helper_functions.dart';
 import 'package:echno_attendance/utilities/styles/padding_style.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -24,13 +26,17 @@ class EmployeeTaskHomeScreen extends StatefulWidget {
 class _EmployeeTaskHomeScreenState extends State<EmployeeTaskHomeScreen> {
   final _taskProvider = TaskService.firestoreTasks();
 
-  late int _selectedIndex;
+  int? _selectedIndex;
 
   get currentEmployee => widget.currentEmployee;
 
   final TextEditingController _searchController = TextEditingController();
 
-  late Map<TaskStatus, int> taskCounts;
+  bool isLoading = false;
+  int onHoldCount = 0;
+  int onGoingCount = 0;
+  int upComingCount = 0;
+  int completedCount = 0;
 
   @override
   void initState() {
@@ -40,129 +46,106 @@ class _EmployeeTaskHomeScreenState extends State<EmployeeTaskHomeScreen> {
   }
 
   Future<void> _updateTaskCounts() async {
+    setState(() {
+      isLoading = true;
+    });
     final taskCountMap = await _taskProvider.getEmployeeTaskCounts(
         assignedEmployee: currentEmployee.employeeId);
     setState(() {
-      taskCounts = taskCountMap;
+      onHoldCount = taskCountMap[TaskStatus.onHold] ?? 0;
+      onGoingCount = taskCountMap[TaskStatus.inProgress] ?? 0;
+      upComingCount = taskCountMap[TaskStatus.todo] ?? 0;
+      completedCount = taskCountMap[TaskStatus.completed] ?? 0;
+      isLoading = false;
     });
   }
 
   @override
   Widget build(context) {
+    final isDark = EchnoHelperFunctions.isDarkMode(context);
+
     return Scaffold(
-      body: Padding(
-        padding: CustomPaddingStyle.defaultPaddingWithAppbar,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              DateFormat.yMMMMd().format(DateTime.now()),
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Text(
-              'Today',
-              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 20.0,
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: CustomPaddingStyle.defaultPaddingWithAppbar,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    DateFormat.yMMMMd().format(DateTime.now()),
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-            ),
-            const SizedBox(height: EchnoSize.defaultSpace / 2),
-            TextFormField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                labelText: 'Search',
-                hintText: 'Search Task...',
-                prefixIcon: const Icon(Icons.search),
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                      Radius.circular(EchnoSize.borderRadiusLg)),
-                ),
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.clear),
-                  onPressed: () {
-                    setState(() {
-                      _searchController.clear();
-                    });
-                  },
-                ),
-              ),
-              onChanged: (value) {
-                // Trigger search when the user types
-                setState(() {});
-              },
-            ),
-            SizedBox(height: _searchController.text.isEmpty ? 20.0 : 10.0),
-            if (_searchController.text.isEmpty)
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  Text(
+                    'Today',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 20.0,
+                        ),
+                  ),
+                  const SizedBox(height: EchnoSize.defaultSpace / 2),
+                  TextFormField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      labelText: 'Search',
+                      hintText: 'Search Task...',
+                      prefixIcon: const Icon(Icons.search),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(EchnoSize.borderRadiusLg)),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.clear),
+                        onPressed: () {
+                          setState(() {
+                            _searchController.clear();
+                          });
+                        },
+                      ),
+                    ),
+                    onChanged: (value) {},
+                  ),
+                  SizedBox(
+                      height: _searchController.text.isEmpty ? 20.0 : 10.0),
+                  if (_searchController.text.isEmpty)
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          buildCategoryButton(
-                              0, 'On Hold', taskCounts[TaskStatus.onHold] ?? 0),
-                          buildCategoryButton(1, 'Ongoing',
-                              taskCounts[TaskStatus.inProgress] ?? 0),
-                          buildCategoryButton(
-                              2, 'Upcoming', taskCounts[TaskStatus.todo] ?? 0),
-                          buildCategoryButton(3, 'Completed',
-                              taskCounts[TaskStatus.completed] ?? 0),
+                          CupertinoSlidingSegmentedControl(
+                            thumbColor: isDark
+                                ? EchnoColors.selectedNavDark
+                                : EchnoColors.selectedNavLight,
+                            children: {
+                              0: Text('On Hold ($onHoldCount)'),
+                              1: Text('Ongoing ($onGoingCount)'),
+                              2: Text('Upcoming ($upComingCount)'),
+                              3: Text('Completed ($completedCount)'),
+                            },
+                            groupValue: _selectedIndex,
+                            onValueChanged: (int? index) {
+                              setState(() {
+                                _selectedIndex = index;
+                              });
+                            },
+                          ),
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  const SizedBox(height: EchnoSize.spaceBtwItems),
+                  const Divider(height: EchnoSize.dividerHeight),
+                  const SizedBox(height: EchnoSize.spaceBtwItems),
+                  EmployeeTaskStreamWidget(
+                    employeeId: widget.currentEmployee!.employeeId,
+                    taskProvider: _taskProvider,
+                    searchController: _searchController,
+                    selectedIndex: _selectedIndex!,
+                  ),
+                ],
               ),
-            const SizedBox(height: EchnoSize.spaceBtwItems),
-            const Divider(height: EchnoSize.dividerHeight),
-            const SizedBox(height: EchnoSize.spaceBtwItems),
-            EmployeeTaskStreamWidget(
-              employeeId: widget.currentEmployee!.employeeId,
-              taskProvider: _taskProvider,
-              searchController: _searchController,
-              selectedIndex: _selectedIndex,
             ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildCategoryButton(int index, String text, int taskCount) {
-    Brightness themeMode = Theme.of(context).brightness;
-    Color selectedColor = themeMode == Brightness.dark
-        ? EchnoColors.selectedNavDark
-        : EchnoColors.selectedNavLight;
-    Color unselectedColor = EchnoColors.darkGrey;
-    return InkWell(
-      onTap: () {
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        decoration: BoxDecoration(
-          color: _selectedIndex == index ? selectedColor : unselectedColor,
-          border: const Border(
-            left: BorderSide(width: 0.1),
-            right: BorderSide(width: 0.1),
-          ),
-        ),
-        child: Row(
-          children: [
-            Text('$text ($taskCount)',
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      color: EchnoColors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 13.0,
-                    )),
-          ],
-        ),
-      ),
     );
   }
 }
--- lib/task_module/screens/task_home_screen.dart
+++ lib/task_module/screens/task_home_screen.dart
@@ -6,7 +6,9 @@ import 'package:echno_attendance/task_module/services/task_service.dart';
 import 'package:echno_attendance/task_module/utilities/task_status.dart';
 import 'package:echno_attendance/task_module/widgets/task_home_widgets/task_home_header.dart';
 import 'package:echno_attendance/task_module/widgets/task_home_widgets/task_home_stream.dart';
+import 'package:echno_attendance/utilities/helpers/helper_functions.dart';
 import 'package:echno_attendance/utilities/styles/padding_style.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskHomeScreen extends StatefulWidget {
@@ -25,9 +27,14 @@ class TaskHomeScreen extends StatefulWidget {
 class _TaskHomeScreenState extends State<TaskHomeScreen> {
   final taskService = TaskService.firestoreTasks();
 
-  late int _selectedIndex;
+  int? _selectedIndex;
   late SiteOffice siteOffice;
   late Map<TaskStatus, int> taskCounts;
+  bool isLoading = false;
+  int onHoldCount = 0;
+  int onGoingCount = 0;
+  int upComingCount = 0;
+  int completedCount = 0;
 
   @override
   void initState() {
@@ -38,15 +45,24 @@ class _TaskHomeScreenState extends State<TaskHomeScreen> {
   }
 
   Future<void> _updateTaskCounts() async {
+    setState(() {
+      isLoading = true;
+    });
     final taskCountMap = await taskService.getSiteTaskCounts(
         siteOffice: widget.siteOffice.siteOfficeName);
     setState(() {
-      taskCounts = taskCountMap;
+      onHoldCount = taskCountMap[TaskStatus.onHold] ?? 0;
+      onGoingCount = taskCountMap[TaskStatus.inProgress] ?? 0;
+      upComingCount = taskCountMap[TaskStatus.todo] ?? 0;
+      completedCount = taskCountMap[TaskStatus.completed] ?? 0;
+      isLoading = false;
     });
   }
 
   @override
   Widget build(context) {
+    final isDark = EchnoHelperFunctions.isDarkMode(context);
+
     return Scaffold(
       appBar: EchnoAppBar(
         leadingIcon: Icons.arrow_back_ios_new,
@@ -58,82 +74,52 @@ class _TaskHomeScreenState extends State<TaskHomeScreen> {
           style: Theme.of(context).textTheme.headlineSmall,
         ),
       ),
-      body: Padding(
-        padding: CustomPaddingStyle.defaultPaddingWithAppbar,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TaskHomeHeader(siteOffice: siteOffice),
-            const SizedBox(height: EchnoSize.spaceBtwSections),
-            Center(
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: CustomPaddingStyle.defaultPaddingWithAppbar,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  TaskHomeHeader(siteOffice: siteOffice),
+                  const SizedBox(height: EchnoSize.spaceBtwSections),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        buildCategoryButton(
-                            0, 'On Hold', taskCounts[TaskStatus.onHold] ?? 0),
-                        buildCategoryButton(1, 'Ongoing',
-                            taskCounts[TaskStatus.inProgress] ?? 0),
-                        buildCategoryButton(
-                            2, 'Upcoming', taskCounts[TaskStatus.todo] ?? 0),
-                        buildCategoryButton(3, 'Completed',
-                            taskCounts[TaskStatus.completed] ?? 0),
+                        CupertinoSlidingSegmentedControl(
+                          thumbColor: isDark
+                              ? EchnoColors.selectedNavDark
+                              : EchnoColors.selectedNavLight,
+                          children: {
+                            0: Text('On Hold ($onHoldCount)'),
+                            1: Text('Ongoing ($onGoingCount)'),
+                            2: Text('Upcoming ($upComingCount)'),
+                            3: Text('Completed ($completedCount)'),
+                          },
+                          groupValue: _selectedIndex,
+                          onValueChanged: (int? index) {
+                            setState(() {
+                              _selectedIndex = index;
+                            });
+                          },
+                        ),
                       ],
                     ),
+                  ),
+                  const SizedBox(height: EchnoSize.spaceBtwItems),
+                  const Divider(height: EchnoSize.dividerHeight),
+                  const SizedBox(height: EchnoSize.spaceBtwItems),
+                  TaskHomeStreamWidget(
+                    siteOffice: siteOffice.siteOfficeName,
+                    taskService: taskService,
+                    selectedIndex: _selectedIndex!,
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: EchnoSize.spaceBtwItems),
-            const Divider(height: EchnoSize.dividerHeight),
-            const SizedBox(height: EchnoSize.spaceBtwItems),
-            TaskHomeStreamWidget(
-              siteOffice: siteOffice.siteOfficeName,
-              taskService: taskService,
-              selectedIndex: _selectedIndex,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildCategoryButton(int index, String text, int taskCount) {
-    Brightness themeMode = Theme.of(context).brightness;
-    Color selectedColor = themeMode == Brightness.dark
-        ? EchnoColors.selectedNavDark
-        : EchnoColors.selectedNavLight;
-    Color unselectedColor = EchnoColors.darkGrey;
-    return InkWell(
-      onTap: () {
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        decoration: BoxDecoration(
-          color: _selectedIndex == index ? selectedColor : unselectedColor,
-          border: const Border(
-            left: BorderSide(width: 0.1),
-            right: BorderSide(width: 0.1),
-          ),
-        ),
-        child: Row(
-          children: [
-            Text('$text ($taskCount)',
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      color: EchnoColors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 13.0,
-                    )),
-          ],
-        ),
-      ),
     );
   }
 }
